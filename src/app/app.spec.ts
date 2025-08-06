4709d37d59d9faad3a4fbab9c6e11d365e308623
kartikaybisaria <kartikay.bisaria@coditas.com>
2025-08-06 12:39:35 +0530

    Update app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-java');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, This is my Angular project!');
   });
 });
