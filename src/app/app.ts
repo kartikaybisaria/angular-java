688254877237e00fbbebfc56ccb6df03a26cec18
kartikaybisaria <kartikay.bisaria@coditas.com>
2025-08-06 14:18:44 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('only-java);
+  protected readonly title = signal('angular-java);
 }
